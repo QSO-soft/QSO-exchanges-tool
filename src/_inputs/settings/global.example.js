3f8e0bed1f12ce78d296726a45ba4cea91a731dc
Danil00524 <jerry.lewis.green@gmail.com>
2024-06-22 17:40:50 +0400

    up

--- src/_inputs/settings/global.example.js
+++ src/_inputs/settings/global.example.js
@@ -1,33 +1,9 @@
-// Секретная фраза, которая будет использоваться для шифрования ваших приватных данных
-// Эта же фраза будет использоваться и для их расшифровки по этому не удаляйте её!
-// Пример: 'abracadabra209581kdhask'
-export const SECRET_PHRASE = 'abracadabra';
-
-// Приватные данные для экстренных telegram уведомлений
-export const TELEGRAM = {
-  // Создаём тут -> https://t.me/BotFather
-  // Пример: 3351122561:BBAh5G_Xdkljlkf2fkjansfdaskjnfwk1
-  token: {
-    modulesInfo: '', // уведомления про выполнение модулей
-    criticalErrors: '', // уведомления с критическими ошибками, которые возможно нужно исправить самому (например, пополнить баланс)
-  },
-
-  // Узнать можно тут -> https://t.me/getmyid_bot
-  // Пример: [721667338, 721667339]
-  IDs: {
-    modulesInfo: [],
-    criticalErrors: [],
-  },
-};
-
 // Приватные данные от OKX для авто-пополнения
 export const OKX = {
   // Имя аккаунта, который будет использоваться в скриптах
   // Сделан этот выбор, чтобы можно было легко и удобно переключаться между разными ОКХ
-  accountName: 'account-1',
-
-  // Имя аккаунта, который будет использоваться для вывода в сети Aptos
-  aptosAccountName: 'account-2',
+  accountName: '',
+  aptosAccountName: '',
 
   // Указав имейл ОКХ в данном поле,
   // при выполнении модуля okx-collect, балансы всех ОКХ будут перекидываться на него
@@ -62,7 +38,6 @@ export const OKX = {
   proxy: '',
 };
 
-// Приватные данные от Binance для авто-пополнения
 export const BINANCE = {
   // Создать можно тут -> https://www.binance.com/en/binance-api
   secretKeys: {
@@ -74,7 +49,6 @@ export const BINANCE = {
   proxy: '',
 };
 
-// Приватные данные от Bitget
 export const BITGET = {
   apiKey: '',
   secret: '',
@@ -84,7 +58,6 @@ export const BITGET = {
   proxy: '',
 };
 
-// Приватные данные от Gate
 export const GATE = {
   secretKeys: {
     apiKey: '',
@@ -93,6 +66,42 @@ export const GATE = {
 
   proxy: '',
 };
+
+// --------------------------- НЕ ОБЯЗАТЕТЛЬНЫЕ ---------------------------
+
+// Приватные данные для экстренных telegram уведомлений
+export const TELEGRAM = {
+  // Создаём тут -> https://t.me/BotFather
+  // Пример: 3351122561:BBAh5G_Xdkljlkf2fkjansfdaskjnfwk1
+  token: {
+    modulesInfo: '', // уведомления про выполнение модулей
+    criticalErrors: '', // уведомления с критическими ошибками, которые возможно нужно исправить самому (например, пополнить баланс)
+  },
+
+  // Узнать можно тут -> https://t.me/getmyid_bot
+  // Пример: [721667338, 721667339]
+  IDs: {
+    modulesInfo: [],
+    criticalErrors: [],
+  },
+};
+
+// Приватный ключ от Ankr RPC
+// Пример: a70406a97dsakjh27412t9fdashkjdll8w2hkjdnaskda9124ydcaf1505
+export const ANKR_KEY = '';
+
+// Получить ключ можно тут - https://admin.moralis.io/register
+export const MORALIS_KEY = '';
+
+// Приватный ключ от Anti-Catpcha - https://anti-captcha.com/
+export const ANTICAPTCHA_KEY = '';
+
+// Приватный ключ от Capmonster - https://capmonster.cloud/
+export const CAPMONSTER_CAPTCHA_KEY = '';
+
+// Получить ключ можно тут - https://portal.1inch.dev/login
+// Пример: sLsss22wwQsdwasKThqkQOCD2daw
+export const INCH_KEY = '';
 
 // Ваша приватная RPC (не обязательно)
 export const RPC = {
@@ -128,3 +137,7 @@ export const RPC = {
   aptos: '',
   starknet: '',
 };
+// Секретная фраза, которая будет использоваться для шифрования ваших приватных данных
+// Эта же фраза будет использоваться и для их расшифровки по этому не удаляйте её!
+// Пример: 'abracadabra209581kdhask'
+export const SECRET_PHRASE = 'abracadabra';
